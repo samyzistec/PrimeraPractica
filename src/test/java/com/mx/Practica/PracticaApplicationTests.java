bf68f3e87605638e5c253fb0beb89eae51ca56ac
Samuel Perez Zistecatl <samyzistec@gmail.com>
2025-03-03 12:09:08 -0600

    Se sube arquetipo para microservicios

---
+++ src/test/java/com/mx/Practica/PracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.Practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
